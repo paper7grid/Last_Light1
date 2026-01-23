d728196fc37b6590a34482432ee3f653c0b7da13
paper7grid <muddassirsadia123@gmail.com>
2026-01-22 16:37:13 -0800

    the door works!

--- last_light1/Scripts/door.gd
+++ last_light1/Scripts/door.gd
@@ -3,7 +3,7 @@ extends Node3D
 var opened = false
 
 func toggle_door():
-	if $AnimationPlayer.current_animation == null:
+	if $AnimationPlayer.current_animation != "open" and $AnimationPlayer.current_animation != "close" :
 		opened = !opened
 	if !opened:
 		$AnimationPlayer.play("close")
---
+++ last_light1/Scripts/playerinteract.gd
@@ -0,0 +1,11 @@
+extends RayCast3D
+
+func _physics_process(delta: float) -> void:
+	if is_colliding():
+		var hit = get_collider()
+		if hit.name == "door":
+			if Input.is_action_just_pressed("interact"):
+				hit.get_parent().get_parent().get_parent().toggle_door()
+				
+				
+				
