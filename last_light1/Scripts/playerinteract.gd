7be2293a6a54c6969e9ca858f6ca13b7a1d59afd
paper7grid <muddassirsadia123@gmail.com>
2026-01-22 22:23:14 -0800

    added crosshair

--- last_light1/Scripts/playerinteract.gd
+++ last_light1/Scripts/playerinteract.gd
@@ -1,17 +1,26 @@
 extends RayCast3D
-
-func _physics_process(delta: float) -> void:
+@onready var crosshair = get_parent().get_parent().get_node("player_ui/CanvasLayer/crosshair")
+func _physics_process(_delta: float) -> void:
 	if is_colliding():
 		var hit = get_collider()
 		if hit.name == "door":
+			if !crosshair.visible:
+				crosshair.visible = true
 			if Input.is_action_just_pressed("interact"):
 				hit.get_parent().get_parent().get_parent().toggle_door()
 				
 		elif hit.name == "drawer":
+			if !crosshair.visible:
+				crosshair.visible = true
 			if Input.is_action_just_pressed("interact"):
 				hit.get_parent().get_parent().toggle_door()
-			
+		else: 
+			if crosshair.visible:
+				crosshair.visible = false
 				
+	else:
+		if crosshair.visible:
+			crosshair.visible = false
 
 				
 				
